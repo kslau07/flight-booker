e7aab2cd691603e1c5723d914bbfe2f60c50b584
kslau07 <abluecar@gmail.com>
2023-06-24 18:32:53 -0400

    Save changes
    Allow user to select 'ANY' airport in query
    
    Show # of results returned

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_21_223826) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_24_204723) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_21_223826) do
     t.integer "arrival_airport_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "seats_avail"
     t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 num_flights_to_seed = 15
 
+def rand_seats_avail
+  [rand(3..12), rand(1..45)].sample
+end
+
+def rand_flight_duration
+  rand(1..7).to_s + "h" + rand(1..59).to_s + "m"
+end
+
 airports = %i[JFK MIA LAX SEA DEN ANC]
 Airport.create!(code: airports.pop) until airports.empty?
 
@@ -16,7 +24,8 @@ end
 
 num_flights_to_seed.times do
   Flight.create!(start: Time.now,
-                 flight_duration: '1',
+                 flight_duration: rand_flight_duration,
+                 seats_avail: rand_seats_avail,
                  departure_airport_id: random_departure_airport,
                  arrival_airport_id: random_arrival_airport)
 end
