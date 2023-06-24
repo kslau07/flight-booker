345dff951a89f047a7b76b7dc05a06a96f870ca3
kslau07 <abluecar@gmail.com>
2023-06-24 01:54:16 -0400

    Add functional form for querying flights
    Make form "remember" query selections
    Update flight dates to today onward
    Fix padding for table head

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,21 +1,28 @@
 class FlightsController < ApplicationController
-  # before_action :update_flight_dates, only: [:index]
+  before_action :update_flight_dates, only: [:index]
   
   def index
-    @flights = Flight.all
     @airports = Airport.all
-    # @flight_search = Flight.new(flight_params)
+    @flights = Flight.all
+
+    # sql query that will collect an array of flight dates
+    @flight_dates = Flight.select(:start).order(start: :asc)
+    @flight_months = @flights.map { |d| d.start.strftime('%B') }.uniq
+    @flight_days = @flights.map { |d| d.start.strftime('%d') }.uniq
+    @flight_years = @flights.map { |d| d.start.strftime('%Y') }.uniq
+    @query = search_params
+    @results = Flight.search_by_fields(search_params)
   end
 
   private
 
-  def flight_params
-    params.require(:flight).permit(:travelers)
+  def search_params
+    params.permit(:departure_code, :arrival_code, :num_tickets, :month, :day, :year)
   end
 
+  # Change flight dates to be from today and onward
   def update_flight_dates
     Flight.count.times do |i|
-      # Flight.find(i+1).update(start: Time.now + rand(1..45).days)
       Flight.find(i+1).update(start: Time.now + (i+1).days)
     end
   end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,38 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+
+  def self.search_by_fields(query)
+    return if query.empty?
+
+    query = sanitize_sql(query)
+
+    dep_aiport_id = Airport.find_by(code: query[:departure_code]).id
+    arr_airport_id = Airport.find_by(code: query[:arrival_code]).id
+
+    # debugger                # delete me
+    
+    Flight.where('departure_airport_id = ?
+                  AND arrival_airport_id = ?', dep_aiport_id, arr_airport_id)
+
+    # Flight.where('departure_airport_id = ?
+    #   AND arrival_airport_id = ?
+    #   AND departure_airport_id = ?
+    #   AND departure_airport_id = ?', x, y, x, y)
+  
+    # Lets test if departs from DEN can be returned
+
+    # debugger if !query.empty?
+
+    # sql = fields.map {|field| "#{field} LIKE ?"}.join(' OR ')
+    # parameters = fields.map {"%#{q}%"}
+    # where(sql, *parameters)
+  end
 end
+
+<<~DELETE
+  delete me
+  irb(main):020:0> Airport.where('code = ?', 'MIA').pluck(:id).first
+  irb(main):031:0> Airport.find_by(code: 'MIA').id
+
+DELETE
