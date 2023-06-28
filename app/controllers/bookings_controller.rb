4b227e0807563689ee487402c11c7b5673953943
sam <abluecar@gmail.com>
2023-06-28 18:50:16 -0400

    Finish manual forms, errors, purchase page
    Manually add error highlighting on form
    To do: Finish styling bookings#show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,16 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
-    @num_tickets = params[:num_tickets].to_i <= 4 ? params[:num_tickets].to_i : 4
+    @num_tickets = num_tickets
     @flight = Flight.find(url_params[:flight_id])
   end
 
   def create
+    @booking_params = booking_params
     @booking = Booking.new(booking_params)
     @num_tickets = booking_params[:num_tickets]
     @flight = Flight.find(booking_params[:flight_id])
-    
+
     if @booking.save
       redirect_to booking_path(@booking)
     else
@@ -19,16 +20,29 @@ class BookingsController < ApplicationController
 
   def show
     @booking = Booking.find(params[:id])
+    
   end
 
   private
 
+  def num_tickets
+    num = url_params[:num_tickets].to_i
+    case num
+    when -Float::INFINITY..0
+      1
+    when 4..Float::INFINITY
+      4
+    when 1..3
+      num
+    end
+  end
+
   def url_params
-    params.permit(:flight_id)
+    params.permit(:flight_id, :num_tickets)
   end
 
   def booking_params
     params.require(:booking).permit(:flight_id, :num_tickets,
-                                    passengers_attributes: [:id, :name])
+                                    passengers_attributes: [:id, :name, :email])
   end
 end
